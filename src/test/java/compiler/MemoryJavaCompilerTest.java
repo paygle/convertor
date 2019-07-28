011867c1707f3fb6af804495dee4114d21487118
paygle <dodar@qq.com>
2019-07-28 11:34:54 +0800

    update test

--- src/test/java/compiler/MemoryJavaCompilerTest.java
+++ src/test/java/compiler/MemoryJavaCompilerTest.java
@@ -1,10 +1,13 @@
+package compiler;
+
 import com.dodar.compiler.MemoryJavaCompiler;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import static org.junit.Assert.assertEquals;
+import static com.dodar.utils.Log.logger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MemoryJavaCompilerTest {
     private final static MemoryJavaCompiler compiler = new MemoryJavaCompiler();
@@ -16,6 +19,7 @@ public class MemoryJavaCompilerTest {
                 + "\treturn \"Hello \" + name;\n" + "}\n}\n";
         final Method greeting = compiler.compileStaticMethod("greeting", "Solution", source);
         final Object result = greeting.invoke(null, "soulmachine");
+        logger.info(result.toString());
         assertEquals("Hello soulmachine", result.toString());
     }
 }
